5fbd0ce8a8b4c7540dee2387123a7180d957b945
YuanRui <864722827@qq.com>
2020-07-10 11:58:58 +0800

    登录模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,25 +1,58 @@
 <template>
   <div class="login">
-    <div class="left"></div>
-    <img src="@/img/login_right.png" alt="" />
+    <div class="left">
+      <div class="title">
+        <img src="@/img/title_logo.png" alt />
+        <span class="titleName">黑马面面</span>
+        <span class="titleLine">|</span>
+        <span class="titleName2">用户登陆</span>
+      </div>
+    </div>
+    <div class="right">
+      <img src="@/img/login_banner_ele.png" alt />
+    </div>
   </div>
 </template>
-
 <script>
-export default {}
+export default {
+  name: 'login',
+}
 </script>
-
 <style lang="less">
 .login {
-  background: linear-gradient(to right, #1494fa, #01c6fa);
-  height: 100%;
+  /* 弹性盒子布局 */
   display: flex;
-  align-items: center;
+  /* 水平布局 */
   justify-content: space-around;
+  /* 垂直居中 */
+  align-items: center;
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
   .left {
     width: 478px;
     height: 550px;
-    background: #f5f5f5;
+    padding: 42px;
+    background-color: #f5f5f5;
+    .title {
+      display: flex;
+      align-items: center;
+      .titleName {
+        font-size: 24px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+        margin: 0 15px;
+      }
+      .titleName2 {
+        font-size: 22px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+        margin: 0 15px;
+      }
+    }
   }
 }
 </style>
