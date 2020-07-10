178df948cf2eac087c3b8e330fbafa585ecc558d
YuanRui <864722827@qq.com>
2020-07-10 14:28:59 +0800

    登录模块 - 输入框

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="login">
     <div class="left">
-      <div class="title">
+      <div class="header">
         <img src="@/img/title_logo.png" alt />
-        <span class="titleName">黑马面面</span>
-        <span class="titleLine">|</span>
-        <span class="titleName2">用户登陆</span>
+        <span class="headerName">黑马面面</span>
+        <span class="headerLine">|</span>
+        <span class="headerName2">用户登陆</span>
+      </div>
+      <div id="app">
+        <el-input
+          class="inputValue"
+          placeholder="请输入用户名"
+          prefix-icon="el-icon-user"
+          v-model="input1"
+        >
+        </el-input>
+        <el-input
+          class="inputValue"
+          show-password
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+          v-model="input2"
+        >
+        </el-input>
+        <el-input
+          class="inputValua"
+          placeholder="请输入验证码"
+          prefix-icon="el-icon-key"
+          v-model="input3"
+        >
+        </el-input>
       </div>
     </div>
     <div class="right">
@@ -16,6 +40,13 @@
 <script>
 export default {
   name: 'login',
+  data() {
+    return {
+      input1: '',
+      input2: '',
+      input3: '',
+    }
+  },
 }
 </script>
 <style lang="less">
@@ -37,20 +68,36 @@ export default {
     height: 550px;
     padding: 42px;
     background-color: #f5f5f5;
-    .title {
+    .header {
       display: flex;
       align-items: center;
-      .titleName {
+      .headerName {
         font-size: 24px;
         font-weight: 400;
         color: rgba(12, 12, 12, 1);
         margin: 0 15px;
       }
-      .titleName2 {
+      .headerName2 {
         font-size: 22px;
         font-weight: 400;
         color: rgba(12, 12, 12, 1);
         margin: 0 15px;
+      }
+    }
+    #app {
+      margin-top: 28px;
+      .inputValue {
+        width: 394px;
+        height: 40px;
+        background: #ffffff;
+        border-radius: 4px;
+        margin-bottom: 25px;
+      }
+      .inputValua {
+        width: 284px;
+        height: 40px;
+        background: #ffffff;
+        border-radius: 4px;
       }
     }
   }
