8bb28952037186009c746b24c566593cf78976b3
YuanRui <864722827@qq.com>
2020-07-10 10:20:40 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 Vue.config.productionTip = false
 import { router } from '@/router/index.js';
 
+Vue.use(ElementUI);
 new Vue({
     router,
     render: h => h(App),
